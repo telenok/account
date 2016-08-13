4eedfb20d55be9a72704058393fae09c9359cef4
Konstantin <pilot911@yandex.ru>
2016-08-13 21:26:56 +0300

    New commit

--- resources/package-process/composer-subscript.2000.php
+++ resources/package-process/composer-subscript.2000.php
@@ -1,5 +1,6 @@
 <?php
 
-    \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
-    \Telenok\Core\Support\Install\ComposerScripts::addListener('\App\Vendor\Telenok\Account\Event\Listener');
-    \Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\CoreServiceProvider');
+\Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
+\Telenok\Core\Support\Install\ComposerScripts::addListener('\App\Vendor\Telenok\Account\Event\Listener');
+\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountServiceProvider');
+\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountDeferredServiceProvider');
