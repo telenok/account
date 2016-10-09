3b4baaa1a466938a401fe08a1a2a0e0dfd6f90ae
Konstantin <pilot911@yandex.ru>
2016-10-10 02:26:07 +0300

    New commit

--- resources/app/Telenok/Account/AccountDeferredServiceProvider.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Vendor\Telenok\Account;
-
-/**
- * @class App.Telenok.Account.AccountServiceProvider
- * Core service provider.
- * @extends Telenok.Account.AccountServiceProvider
- */
-class AccountDeferredServiceProvider extends \Telenok\Account\AccountDeferredServiceProvider {}
--- resources/package-process/composer-subscript.2000.php
+++ resources/package-process/composer-subscript.2000.php
@@ -3,4 +3,3 @@
 \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
 \Telenok\Core\Support\Install\ComposerScripts::addListener('\App\Vendor\Telenok\Account\Event\Listener');
 \Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountServiceProvider');
-\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountDeferredServiceProvider');
