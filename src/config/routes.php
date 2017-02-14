e1d6754d3a8e839da85c122edc2e236c5a76f93f
Konstantin <pilot911@yandex.ru>
2017-02-14 17:52:39 +0300

    New features

--- src/config/routes.php
+++ src/config/routes.php
@@ -4,13 +4,8 @@ app('router')->post('personal/account/login', 'App\Vendor\Telenok\Account\Widget
 app('router')->get('personal/account/logout', 'App\Vendor\Telenok\Account\Widget\Login\Controller@getLogout')->name('telenok.account.logout');
 app('router')->post('personal/account/password-restore', 'App\Vendor\Telenok\Account\Widget\Login\Controller@postLogin')->name('telenok.account.password-restore');
 
-app('router')->get('personal/account/redirect/social-network/{name}', 'App\Vendor\Telenok\Account\Widget\Login\Controller@redirectToProvider')
-    ->name('telenok.account.redirect-to-provider')
-    ->middleware('web');
-
-app('router')->get('personal/account/login/social-network/callback/{name}', 'App\Vendor\Telenok\Account\Widget\Login\Controller@handleProviderCallback')
-    ->name('telenok.account.handle-provider-callback')
-    ->middleware('web');
+app('router')->get('personal/account/redirect/social-network/{name}', 'App\Vendor\Telenok\Account\Widget\Login\Controller@redirectToProvider')->name('telenok.account.redirect-to-provider');
+app('router')->get('personal/account/login/social-network/callback/{name}', 'App\Vendor\Telenok\Account\Widget\Login\Controller@handleProviderCallback')->name('telenok.account.handle-provider-callback');
 
 app('router')->post('personal/account/password-reset', 'App\Vendor\Telenok\Account\Widget\ResetPassword\Controller@postResetLinkEmail')->name('telenok.account.reset');
 app('router')->get('personal/account/password-reset/process/{token}', 'App\Vendor\Telenok\Account\Widget\ResetPassword\Controller@redirectToResetForm')->name('telenok.account.reset.process');
