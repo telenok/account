939975b494dbe6315543d89b8c5f6dd0d4c9a73e
Konstantin <pilot911@yandex.ru>
2017-03-16 01:17:30 +0300

    New features

--- resources/package-process/composer-subscript.2000.php
+++ resources/package-process/composer-subscript.2000.php
@@ -1,5 +1,5 @@
 <?php
 
 \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
-\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountServiceProvider');
-\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\SocialiteServiceProvider');
+\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\AccountServiceProvider::class');
+\Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\Account\SocialiteServiceProvider::class');
